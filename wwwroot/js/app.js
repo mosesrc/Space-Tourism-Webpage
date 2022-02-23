f4de0afbb741840dae26e095d30d1ffbd7b69557
mosesrc <ryanchristophermoses@gmail.com>
2022-02-23 18:18:06 -0500

    Finish Explore Button

---
+++ wwwroot/js/app.js
@@ -0,0 +1,13 @@
+﻿// Hover effect on the Explore button
+function btnOverlay() {
+    const btn = document.querySelector('.explore-btn h5 a');
+    let overlay = document.querySelector('.circle-overlay');
+
+    btn.addEventListener("mouseenter", () => {
+        overlay.style.opacity = '0.1';
+    });
+
+    btn.addEventListener("mouseleave", () => {
+        overlay.style.opacity = "0";
+    });
+}
