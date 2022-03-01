dd87150913d8c06c54d2cbd75d32dee618deb18b
mosesrc <ryanchristophermoses@gmail.com>
2022-02-28 22:31:41 -0500

    Complete Data Models for each Data JSON file

---
+++ Model/CrewMember.cs
@@ -0,0 +1,23 @@
+﻿namespace SpaceTourismWebSite.Model
+{
+    public class CrewMember { 
+    public class Rootobject
+    {
+        public Crew[] crew { get; set; }
+    }
+
+    public class Crew
+    {
+        public string name { get; set; }
+        public Images images { get; set; }
+        public string role { get; set; }
+        public string bio { get; set; }
+    }
+
+    public class Images
+    {
+        public string png { get; set; }
+        public string webp { get; set; }
+    }
+}
+}
---
+++ Model/Equipment.cs
@@ -0,0 +1,23 @@
+﻿namespace SpaceTourismWebSite.Model
+{
+    public class Equipment
+    {
+        public class Rootobject
+        {
+            public Technology[] technology { get; set; }
+        }
+
+        public class Technology
+        {
+            public string name { get; set; }
+            public Images images { get; set; }
+            public string description { get; set; }
+        }
+
+        public class Images
+        {
+            public string portrait { get; set; }
+            public string landscape { get; set; }
+        }
+    }
+}
